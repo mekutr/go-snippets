4b599ed1fa1c41bf9de98c4eead25abcb7bcbe54
mekutr <mekutr@gmail.com>
2018-12-09 03:04:01 -0600

    updates on snippet number 31

--- 31_race_conditions.go
+++ 31_race_conditions.go
@@ -8,7 +8,7 @@ import (
 /*
 -When multiple goroutines are not synchronized while reading/writing from/to a shared resource,
  they might attempt to read and write to that resource without waiting each others' turn. This
- condition is called a race condition.
+ condition is called the race condition.
 -Goroutines do not respect the atomic transactions that the other goroutines has to handle if
  the goroutines are not designed in that respect. If the atomic transactions that each goroutine
  has to handle is not identified while designing the software, it introduces unwanted bugs into
@@ -17,12 +17,12 @@ import (
 
 /*
 -Following example introduces a race condition between two goroutines
--In this case, second goroutine will not respect the first goroutine and it will try to increment
- the counter as soon as it takes its turn. While the expected output is 10000 after the first goroutine
- is finished executing and 20000 after the second goroutine is finished executing, it will always create
- different results after the following is executed.
--The reason is because there is no atomicity controller between two goroutines to make the second one
- wait until the first one finishes.
+-In this case, the second goroutine will not respect the first goroutine and it will try to increment
+ the counter as soon as it takes its turn. While the expected output is 20000 (10000 + 10000) from
+ one of the goroutines, it will always create different results.
+-There is no synchronization between two goroutines to make them wait until the current one finishes
+ its operation on the given variable. Instead, they end up overwriting each others' results and
+ eventually it creates a race condition.
 */
 
 var counter int
